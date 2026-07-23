58c8b0f50d72c8f245c350e8020304ff35b69050
Sreya Reddy <reddysrey07@gmail.com>
2026-07-23 19:36:12 +0530

    Initial project structure and Day 1 Python basics

---
+++ 01_Python_Fundamentals/01_Python_Basics/practice.py
@@ -0,0 +1,12 @@
+# ============================================
+# Day 1 - Python Basics
+# Name: Sreya Reddy
+# ============================================
+
+print("Hello, Python!")
+
+name = "Sreya"
+age = 24
+
+print("Name:", name)
+print("Age:", age)
