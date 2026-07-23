c7d7ff684e5b472a4ca927d59f903eeeda4bcce6
Sreya_K <143612061+Sreya-Kesav@users.noreply.github.com>
2026-07-23 19:55:58 +0530

    Update practice.py

--- 01_Python_Fundamentals/01_Python_Basics/practice.py
+++ 01_Python_Fundamentals/01_Python_Basics/practice.py
@@ -1,7 +1,7 @@
-# ============================================
+
+# ==========================================
 # Day 1 - Python Basics
-# Name: Sreya Reddy
-# ============================================
+# ==========================================
 
 print("Hello, Python!")
 
@@ -10,3 +10,27 @@ age = 24
 
 print("Name:", name)
 print("Age:", age)
+
+# Data Types
+
+country = "India"
+
+print(country)
+print(type(country))
+
+# Arithmetic Operators
+
+print(10 + 5)
+print(20 - 5)
+print(5 * 4)
+print(20 / 4)
+print(103 // 10)
+print(103 % 10)
+print(2 ** 5)
+
+# Comparison Operators
+
+print(10 > 5)
+print(10 >= 10)
+print(5 < 3)
+print(8 != 9)
